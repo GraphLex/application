ae4812a37c985d9b46b0bd4b6976e59808d527fc
Rhys Sharpe <davidrsharpe@southern.edu>
2026-01-21 10:39:26 -0500

    adding info file

---
+++ pages/Info.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("About GraphLex")
+
+st.markdown("Created by Rhys Sharpe, Rhett Seitz, and Germán H. Alférez, 2025-2026. CC-BY-NC 4.0. \n\n Underlying datasets: BHSA (Eep Talstra Center for Bible and Computer, VU University Amsterdam, "\
+                   "OHB (Eliran Wong), and Nestle 1904 (Center for Biblical Languages and Computing, Andrews University.\n\n For source code, visit https://github.com/graphlex/application.")
